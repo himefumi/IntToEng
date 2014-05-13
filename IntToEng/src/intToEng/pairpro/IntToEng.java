260b6f12810dbf1fbb8d0c0641e0bfb3c932d8dc
g10913hf <g10913hf@m20204.tsuda.ac.jp>
2014-05-13 16:18:24 +0900

    完成です

--- IntToEng/src/intToEng/pairpro/IntToEng.java
+++ IntToEng/src/intToEng/pairpro/IntToEng.java
@@ -1,5 +1,5 @@
 package intToEng.pairpro;
-import java.util.Scanner;
+import java.util.*;
 
 public class IntToEng {
 	 public static void main(String[] args) {
@@ -10,36 +10,45 @@ public class IntToEng {
 	 
 		    static String tlanslateEng(String n) {
 		    	// ƒ‰ƒCƒuƒ‰ƒŠW
-		    	String[] first = {
-		    			"zero","one","two","three","four","five",
-		    			"six","seven","eight","nine","twenty"
-		    	};
-		    	String[] ten = {
-		    			"ten","elevn","twelve","therteen","fourteen","fifteen",
-		    			"sixteen","seventeen","eighteen","nineteen"	
-		    	};
-		    	String[] second = {
-		    			"","","twenty","thirty","fourty","fifty",
-		    			"sixty","seventy","eighty","ninety"
-		    	};
-		    	String[] hundred = { "hundred","hundreds" };
+		    	String[][] getNumber = { // 1‚ÌˆÊ
+		    			{
+		    				"zero","one","two","three","four","five",
+		    				"six","seven","eight","nine"
+		    			},{ // 10‚ÌˆÊ
+		    				"","","twenty","thirty","fourty","fifty",
+		    				"sixty","seventy","eighty","ninety"
+		    			},{ 
+		    				"one-hundred","two-hundreds","third-hundreds","four-hundreds","five-hundreds",
+		    				"six-hundreds","seven-hundreds","eight-hundreds","nine-hundreds"
+		    		    },{  // 10‚ÌŒ…
+		    		    	"ten","elevn","twelve","therteen","fourteen","fifteen",
+		    		    	"sixteen","seventeen","eighteen","nineteen"	
+		    		    }};
 		    	
 		    	int count = 0;
-		    	int[] cn = new int(3);
-		    	char[] change = new char(3);
-		    	String result;
+		    	int[] cn = new int[n.length()];
+		    	if(cn.length > 3) 
+		    	    System.err.println("3Œ…‚Ü‚Å‚Ì“ü—Í");
+		    	char[] change = new char[3];
+		    	String result = "";
 		    	
 		    	for(int i=0; i<n.length(); i++) {
-		    		change[i]=n.charAt(n.length()-1-i);
+		    	    change[i]=n.charAt(n.length()-1-i);
+		    	    cn[i] = Integer.parseInt(""+change[i]);
 		    	}
-		    	while(count<3 && change[count]!=null) {
-		    		cn[count] = Intger.parseInt(change[2-count]);
-		    		result = first[cn[count]] + result;
+		    	while(count < n.length()) {
+		    	    if(count == 0 && cn[1] == 1) {
+		    		result = getNumber[3][cn[count]];
 		    		count++;
+		    	    } else if(count == 1) {
+		    		result = getNumber[count][cn[count]] + " " + result;
+		    	    } else if(count == 2){
+		    		result = getNumber[count][cn[count]-1] + " " + result;
+		    	    } else {
+		    		result = getNumber[count][cn[count]] + " " + result;
+		    	    }
+		    	    count++;
 		    	}
-		    	
-		    	
-			return result;
+		    	return result;
+		        }   
 		    }
-
-}
